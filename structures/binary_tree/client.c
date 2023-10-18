745f49e33d0f6922fa898335a47b99e733668587
LUCAS EMANOEL BRAGA NEIVA <0030482221012@lti.fatecso.edu.br>
2023-10-18 11:04:46 -0300

    fix

--- structures/binary_tree/client.c
+++ structures/binary_tree/client.c
@@ -2,34 +2,42 @@
 #include "binary_tree.h"
 
 int main() {
-    TBinaryTree tree;
+  TBinaryTree tree;
 
-    initBinaryTree(&tree);
+  initBinaryTree(&tree);
 
-    
-    insertNode(&tree, 4);
-    insertNode(&tree, 6);
-    insertNode(&tree, 0);
-    insertNode(&tree, 9);
-    insertNode(&tree, 8);
-    insertNode(&tree, 7);
-    insertNode(&tree, 5);
-    insertNode(&tree, 2);
-    insertNode(&tree, 3);
-    insertNode(&tree, 1);
-    
-    
-    printf("Pre order traversal: ");
-    preOrderTraversal(tree.root);
-    printf("\n");
-    
-    printf("In order traversal: ");
-    inOrderTraversal(tree.root);
-    printf("\n");
+  insertNode(&tree, 4);
+  insertNode(&tree, 6); 
+  insertNode(&tree, 0);
+  insertNode(&tree, 9);
+  insertNode(&tree, 8);
+  insertNode(&tree, 7);
+  insertNode(&tree, 5);
+  insertNode(&tree, 2);
+  insertNode(&tree, 3);
+  insertNode(&tree, 1);   
 
-    printf("Post order traversal: ");
-    postOrderTraversal(tree.root);
-    printf("\n");
+  // Search for a node
+  TTreeNode* node = search(&tree, 8);
+  if (node != NULL) {
+    printf("Found node with value %d\n", node->data);
+  } else {
+    printf("Node not found\n"); 
+  }
 
-    freeBinaryTree(tree.root);
+  // Delete a node
+  deleteNode(&tree, 0);
+
+  // Traversals
+  printf("In order traversal: ");
+  inOrderTraversal(tree.root);
+  printf("\n");
+
+  printf("Post order traversal: ");
+  postOrderTraversal(tree.root);
+  printf("\n");  
+
+  freeBinaryTree(tree.root);
+
+  return 0;
 }
